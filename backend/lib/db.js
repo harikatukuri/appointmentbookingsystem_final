6d1419ffd99d3dd87c7f528778fdc6e2ca8d1859
Harika Atukuri <atukuriharika2@gmail.com>
2025-10-30 23:29:49 +0530

    Refactor MongoDB connection handling
    Refactor MongoDB connection logic to check for existing connection before attempting to connect. Update connection URI to use MongoDB Atlas.

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
+let isConnected = false;
 
 const connectDB = async () => {
-  try {
-    const mongoURI = "mongodb://localhost:27017/appointmentbooking";
+  if (isConnected) {
+    console.log("🔁 Using existing MongoDB connection");
+    return;
+  }
 
+  try {
+    const mongoURI = "mongodb+srv://harika_db_user:VC1WBCrPb0HyuOKb@cluster0.ls18rx6.mongodb.net/?appName=Cluster0";
     const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
+    isConnected = conn.connections[0].readyState;
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection failed: ${error.message}`);
-    process.exit(1);
+    throw error;
   }
 };
 
