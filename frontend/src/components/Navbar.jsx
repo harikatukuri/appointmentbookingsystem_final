3ed40802c9eaa5a95c9687e673afcc8bfae0dab9
Harika Atukuri <atukuriharika2@gmail.com>
2025-10-31 10:41:02 +0530

    Use useNavigate for logout redirection
    Replaced window.location.href with useNavigate for logout.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export default function Navbar() {
+  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   console.log(username);
@@ -26,7 +27,7 @@ export default function Navbar() {
     localStorage.removeItem("token");
     setIsLoggedIn(false);
     setUsername("");
-    window.location.href = "/";
+    navigate('/');
   };
 
   return (
